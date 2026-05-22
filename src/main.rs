f8b2f83add7e5470b7704f79b2d0c63bdbcd5b9d
Aaronphilip2003 <aaronphilip2003@gmail.com>
2026-05-22 16:35:09 +0530

    using walkdir to list out files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use walkdir::WalkDir;
 fn main() {
-    
+    let path = "/Users/aaron/Downloads";
+
+    for entry in WalkDir::new(path) {
+        let entry = entry.unwrap();
+        println!("{}",entry.path().display());
+    }
 }
