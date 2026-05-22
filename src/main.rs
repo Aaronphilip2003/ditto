49a3e1f3f3cf8a2f4d484b16fa5d4a7d341a44cc
Aaronphilip2003 <aaronphilip2003@gmail.com>
2026-05-22 18:07:08 +0530

    making and displaying hashes of all files in that folder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::env;
 use walkdir::WalkDir;
+use sha2::{Digest, Sha256};
+use std::path::Path;
+use hex::encode;
+use std::fs::File;
+use std::io::{self, Read};
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,7 +21,20 @@ fn main() {
         let entry = entry.unwrap();
 
         if entry.file_type().is_file() {
-            println!("{}", entry.path().display());
+            let file_path = entry.path();
+            let hash = hash_file(file_path).unwrap();
+            println!("{}: {}", file_path.display(), hash);
         }
     }
+
+}
+
+fn hash_file(path: &Path) -> io::Result<String> {
+    let mut file = File::open(path)?;
+
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+
+    let hash = Sha256::digest(&buffer);
+    Ok(encode(hash))
 }
